0d8a1a5041ab3594570fb21b0aa4ad07cbb9b5d0
Darren Chang <darrenchang951@gmail.com>
2017-02-06 14:45:34 +1100

    Initial setup

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public GameObject particle;
+
+	[SerializeField]
+	private float speed;
+	bool start;
+	bool gameOver;
+	Rigidbody rb;
+
+	void Awake() {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		start = false;
+		gameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!start) {
+			if (Input.GetMouseButtonDown (0)) {
+				rb.velocity = new Vector3 (speed, 0, 0);
+				start = true;
+			}
+		}
+
+		Debug.DrawRay (transform.position, Vector3.down, Color.red);
+
+		if (!Physics.Raycast (transform.position, Vector3.down, 1f)) {
+			gameOver = true;
+			rb.velocity = new Vector3 (0, -25f, 0);
+
+			Camera.main.GetComponent<CameraFollow> ().gameOver = true;
+		}
+
+		if (Input.GetMouseButtonDown (0) && !gameOver) {
+			SwitchDirection ();
+		}
+	}
+
+	void SwitchDirection() {
+		if (rb.velocity.z > 0) {
+			rb.velocity = new Vector3 (speed, 0, 0);
+		} else if (rb.velocity.x > 0) {
+			rb.velocity = new Vector3 (0, 0, speed);
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Diamond") {
+			GameObject part = Instantiate (particle, col.gameObject.transform.position, Quaternion.identity) as GameObject;
+			Destroy (col.gameObject);
+			Destroy (part, 1f);
+		} 
+	}
+}
---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour {
+
+	public GameObject platform;
+	public GameObject diamond;
+
+	Vector3 lastPos;
+	float size;
+	public bool gameOver;
+
+	// Use this for initialization
+	void Start () {
+		lastPos = platform.transform.position;
+		size = platform.transform.localScale.x;
+
+		for (int i = 0; i < 20; i++) {
+			SpawnPlatforms();
+		}
+
+		InvokeRepeating ("SpawnPlatforms", 2f, 0.2f);
+	}
+
+	void SpawnPlatforms() {
+		int rand = Random.Range (0, 6);
+		if (rand < 3) {
+			SpawnX ();
+		} else if (rand >= 3) {
+			SpawnZ ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOver) {
+			CancelInvoke("SpawnPlatforms");
+		}
+	}
+
+	void SpawnX() {
+		Vector3 pos = lastPos;
+		pos.x += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+
+		int rand = Random.Range (0, 4);
+		if (rand < 1) {
+			Instantiate (diamond, new Vector3(pos.x, pos.y + 1, pos.z), diamond.transform.rotation);
+		}
+	}
+
+	void SpawnZ() {
+		Vector3 pos = lastPos;
+		pos.z += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+
+		int rand = Random.Range (0, 4);
+		if (rand < 1) {
+			Instantiate (diamond, new Vector3(pos.x, pos.y + 1, pos.z), diamond.transform.rotation);
+		}
+	}
+}
